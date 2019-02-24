49416d35e079bcd3e6b4f63dba5ea7325b3d22b8
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2019-02-24 00:11:06 -0600

    Bash version works

--- src/zcli.sh
+++ src/zcli.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+URL_BASE="https://sandbox.zamzar.com/"
+JOBS="v1/jobs/"
+FILES="v1/files/"
+FILES2="/content"
+
 # Test number of parameters
 if [ $# -ne 4 ]; then
     echo "Usage: ./zcli [key file] [input format] [output format] [input file]"
@@ -9,7 +14,7 @@ fi
 KEY=$1
 INPUT=$2
 OUTPUT=$3
-FILE=$4
+FILE=$(pwd)/$4
 
 # Test if key file exists
 if [ ! -r $KEY ]; then
@@ -30,3 +35,49 @@ echo "key: $KEY"
 echo "input format: $INPUT"
 echo "output format: $OUTPUT"
 echo "file to convert: $FILE"
+
+# Upload file to server
+RESPONSE=$(curl $URL_BASE$JOBS \
+    -u $KEY \
+    -X POST \
+    -F "source_file=@$FILE" \
+    -F "target_format=$OUTPUT")
+
+# Remove braces and brackets
+RESPONSE=$(echo $RESPONSE | sed 's/[]{}\[]//g')
+
+# Extract job ID
+JOB_ID=$(echo $RESPONSE | awk 'BEGIN{FS=","} {print $1}')
+JOB_ID=$(echo $JOB_ID | awk 'BEGIN{FS=":"} {print $2}')
+echo "[!] job id: $JOB_ID"
+
+JOB_STATUS=""
+while [ "$JOB_STATUS" != "successful" ]; do
+    # Check if job is complete
+    RESPONSE=$(curl $URL_BASE$JOBS$JOB_ID \
+        -u $KEY)
+    
+    # Remove braces and brackets
+    RESPONSE=$(echo $RESPONSE | sed 's/[]{}\[]//g')
+    
+    # Extract job status
+    JOB_STATUS=$(echo $RESPONSE | awk 'BEGIN{FS=","} {print $3}')
+    JOB_STATUS=$(echo $JOB_STATUS | awk 'BEGIN{FS=":"} {print $2}')
+    
+    # Remove quotes
+    JOB_STATUS=$(echo $JOB_STATUS | sed 's/"//g')
+done
+echo "[+] job complete"
+
+# Extract file ID
+FILE_ID=$(echo $RESPONSE | awk 'BEGIN{FS=","} {print $10}')
+FILE_ID=$(echo $FILE_ID | awk 'BEGIN{FS=":"} {print $3}')
+
+echo "[!] file id: $FILE_ID"
+
+# Download file
+curl $URL_BASE$FILES$FILE_ID$FILES2 \
+    -u $KEY \
+    -L \
+    -O \
+    -J
